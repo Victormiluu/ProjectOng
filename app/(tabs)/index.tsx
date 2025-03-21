259101e4b7ce36852d547fb579c556aa722cfafb
Victormiluu <victormiludeaquino@gmail.com>
2025-03-21 20:28:45 -0300

    initial setup project

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,33 @@
+import { Image, StyleSheet, Platform } from 'react-native';
+
+import { View,Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.containerStyle}>
+      <Image
+        source={require('@/assets/images/autism-logo.png')}
+      />
+      <Text style={styles.textHeaderStyle}>Welcome to my app about autism ONG</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+//ola
+  containerStyle:{
+    flex:1,
+    alignItems:"center",
+    justifyContent:"center"
+  },
+
+  textHeaderStyle: {
+    fontWeight:"bold",
+    fontSize:35
+  },
+
+  logoStyle: {
+
+  }
+  
+});
